675b51d504354019d240bba332a35672c813c4c8
Maarten van den Hooven <maartenv@kontent.ai>
2023-01-09 10:26:12 +0100

    fixed layout

--- src/IntegrationApp.tsx
+++ src/IntegrationApp.tsx
@@ -103,7 +103,9 @@ export const IntegrationApp: FC = () => {
         <textarea value={elementValue} onChange={e => updateValue(e.target.value)} disabled={isDisabled} tabIndex={0} data-id="root" rows={1} placeholder="">
           <textarea tabIndex={0} data-id="root" rows={1} className='embedTextarea'  placeholder=""></textarea>
           </textarea>
-        <button onClick={(e: any) => generateAIContent(elementValue)}><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path></svg></button>
+        <button onClick={(e: any) => generateAIContent(elementValue)}>
+          <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path></svg>
+          </button>
       </section>
     </>
   );
